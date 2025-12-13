44a7ab71a216bfbc6b177cfd0e2a4c7783db613b
eastcoast <princeadewale391@gmail.com>
2025-12-13 16:14:35 +0100

    removed the react import to stop error

--- src/pages/historyPage.tsx
+++ src/pages/historyPage.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from "react";
+import { useEffect, useState } from "react";
 import Layout from "../layouts/layout";
 import type { JSX } from "react";
 import { getHistory } from "../services/user";
