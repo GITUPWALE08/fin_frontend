7d2c08c7f8a24c97bbe5aa60c08a8a837eca52f6
eastcoast <princeadewale391@gmail.com>
2025-12-21 22:20:30 +0100

    axios doesn't support .json so removed

--- src/services/stocks.tsx
+++ src/services/stocks.tsx
@@ -1,7 +1,7 @@
 import { get, post } from "../lib/api";
 
 export const getPrice = (symbol: string) =>
-  get(`api/price?symbol=${symbol}`);
+  get(`/api/price?symbol=${symbol}`);
 
 export const buyStock = (symbol: string, shares: number, password: string) =>
   post("/buy", { symbol, shares, password });
